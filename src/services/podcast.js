511c453b886e97a03a71e150fc4099da1ea9eccb
biocrista96 <60754122+biocrista96@users.noreply.github.com>
2024-05-15 11:40:54 -0400

    fix load episodes service

--- src/services/podcast.js
+++ src/services/podcast.js
@@ -1,46 +1,45 @@
 import axios from "axios";
 import getDataFromXml from "../utils/getDataFromXml";
 
-const allorigins = 'http://api.allorigins.win/get?url=';
+const allorigins = "http://api.allorigins.win/get?url=";
 
 const api = axios.create({
-  baseURL:`${allorigins}https://itunes.apple.com/`,
+  baseURL: `${allorigins}https://itunes.apple.com/`,
 });
 
-export const getTopPodcasts = async () =>{
-  try{
-    const response = await api.get(`us/rss/toppodcasts/limit=100/genre=1310/json`)
-    const responseJson = JSON.parse(response.data.contents)
-    return responseJson
+export const getTopPodcasts = async () => {
+  try {
+    const response = await api.get(
+      `us/rss/toppodcasts/limit=100/genre=1310/json`
+    );
+    const responseJson = JSON.parse(response.data.contents);
+    return responseJson;
+  } catch (error) {
+    throw error;
   }
-  catch(error){
-    throw error
-  }
-}
+};
 
-export const getPodcastDetail = async (id ) =>{
-  try{
-    const response = await api.get(`lookup?id=${id}&media=podcast`)
-    const responseJson = JSON.parse(response.data.contents)
-    return responseJson
+export const getPodcastDetail = async (id) => {
+  try {
+    const response = await api.get(`lookup?id=${id}&media=podcast`);
+    const responseJson = JSON.parse(response.data.contents);
+    return responseJson;
+  } catch (error) {
+    console.error(error);
   }
-  catch(error){
-    console.error(error)
-  }
-}
+};
 
-export const getEpisodes = async (url) =>{
-  try{
-    const responseUrl = await axios.get(`${allorigins}${url}`)
-    console.log(responseUrl)
-    const responseXml = responseUrl.data
-    const response = getDataFromXml(responseXml)
+export const getEpisodes = async (url) => {
+  console.log("hola2");
+  try {
+    console.log("aloooo");
+    const responseUrl = await axios.get(`${url}`);
+    console.log(responseUrl, "hola?");
+    const responseXml = responseUrl.data;
+    const response = getDataFromXml(responseXml);
 
-    return response
+    return response;
+  } catch (error) {
+    console.error(error);
   }
-  catch(error){
-    console.error(error)
-  }
-}
-
- 
+};
--- src/store/PodcastActions.js
+++ src/store/PodcastActions.js
@@ -1,116 +1,113 @@
-import { getTopPodcasts, getPodcastDetail, getEpisodes } from "../services/podcast";
-import  podcastActions from "./actionsConstants";
+import {
+  getTopPodcasts,
+  getPodcastDetail,
+  getEpisodes,
+} from "../services/podcast";
+import podcastActions from "./actionsConstants";
 
+const localStoragePodcasts = "podcastsList";
+const localStorageLastTime = "lastTimeRequested";
 
-const localStoragePodcasts = 'podcastsList';
-const localStorageLastTime = 'lastTimeRequested';
+export const getPodcastsActionCreator = (updateList) => {
+  return async (dispatch) => {
+    dispatch({ type: podcastActions.getPodcastsLoading });
 
-export const getPodcastsActionCreator = (updateList) =>{
-  return async (dispatch)=>{
+    try {
+      let response;
 
-    dispatch({type: podcastActions.getPodcastsLoading})
-
-    try{
-      
-      let response 
-
-      if(updateList) { 
-       const callResponse = await getTopPodcasts()
-       response = callResponse.feed.entry
-       localStorage.setItem(localStoragePodcasts, JSON.stringify(response))
-      }else{
+      if (updateList) {
+        const callResponse = await getTopPodcasts();
+        response = callResponse.feed.entry;
+        localStorage.setItem(localStoragePodcasts, JSON.stringify(response));
+      } else {
         response = JSON.parse(localStorage.getItem(localStoragePodcasts));
-      } 
-
+      }
       dispatch({
         type: podcastActions.getPodcastsSuccess,
-        payload:response
+        payload: response,
       });
-    }
-    catch(error){
+    } catch (error) {
       dispatch({
         type: podcastActions.getPodcastsFailed,
-        payload:error.message
+        payload: error.message,
       });
     }
-  } 
-}
+  };
+};
 
-export const getpodcastDetailActionCreator = (id) =>{
-  return async (dispatch) =>{
-    dispatch({type:podcastActions.getPodcast, payload:id})
-  }
-}
+export const getpodcastDetailActionCreator = (id) => {
+  return async (dispatch) => {
+    dispatch({ type: podcastActions.getPodcast, payload: id });
+  };
+};
 
-export const getEpisodesActionCreator = (id)=>{
-  return async (dispatch) =>{
+export const getEpisodesActionCreator = (id) => {
+  return async (dispatch) => {
+    dispatch({ type: podcastActions.getEpisodesLoading });
 
-    dispatch({type:podcastActions.getEpisodesLoading})
+    try {
+      const response = await getPodcastDetail(id);
 
-    try{
-      const response = await await getPodcastDetail(id);
-      let podcastUrl= response.results[0].feedUrl
+      let podcastUrl = response.results[0].feedUrl;
 
-      if(podcastUrl){
-            try{
+      if (podcastUrl) {
+        console.log("hola");
+        try {
           const response = await getEpisodes(podcastUrl);
+          console.log(response);
           dispatch({
             type: podcastActions.getEpisodesSuccess,
-            payload: response
+            payload: response,
           });
-        }catch(error){
+        } catch (error) {
           dispatch({
             type: podcastActions.getEpisodesFailed,
-            payload: error.message
+            payload: error.message,
           });
         }
       }
-    }catch(error){
+    } catch (error) {
       dispatch({
         type: podcastActions.getEpisodesFailed,
-        payload: error.message
+        payload: error.message,
       });
     }
+  };
+};
 
-
-  }
-}
-
-export const setLastTimeRequestedActionCreator = () =>{
-  return (dispatch)=>{
-    localStorage.setItem(localStorageLastTime, String( new Date()));
+export const setLastTimeRequestedActionCreator = () => {
+  return (dispatch) => {
+    localStorage.setItem(localStorageLastTime, String(new Date()));
     dispatch({
-      type:podcastActions.setLastTimeRequested,
-      payload: new Date()
-    })
-  }
+      type: podcastActions.setLastTimeRequested,
+      payload: new Date(),
+    });
+  };
+};
 
-}
-
-
-export const getLastTimeRequestedActionCreator = () =>{
-  return (dispatch)=>{
+export const getLastTimeRequestedActionCreator = () => {
+  return (dispatch) => {
     const lastTime = localStorage.getItem(localStorageLastTime);
     dispatch({
-      type:podcastActions.getLasTimeRequested,
-      payload: new Date(lastTime)
-    })
-  }
-}
+      type: podcastActions.getLasTimeRequested,
+      payload: new Date(lastTime),
+    });
+  };
+};
 
-export const setEpisodeActionCreator = (id) =>{
-  return (dispatch) =>{
+export const setEpisodeActionCreator = (id) => {
+  return (dispatch) => {
     dispatch({
-      type:podcastActions.setEpisode,
-      payload:id
-    })
-  }
-}
+      type: podcastActions.setEpisode,
+      payload: id,
+    });
+  };
+};
 
-export const cleanEpisodeActionCreator = ()=>{
-  return (dispatch) =>{
+export const cleanEpisodeActionCreator = () => {
+  return (dispatch) => {
     dispatch({
-      type:podcastActions.cleanEpisode,
-    })
-  }
-}
+      type: podcastActions.cleanEpisode,
+    });
+  };
+};
