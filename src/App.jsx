96ddf6d061db8e79d9198ed1eb9d91477955359b
biocrista96 <60754122+biocrista96@users.noreply.github.com>
2024-05-15 12:15:46 -0400

    remove external import from scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import {BrowserRouter as Router,Route,Routes } from "react-router-dom";
-import './App.scss';
-import HomeLayout from './layout/HomeLayout/HomeLayout';
-import PodcastList from './views/PodcastsList/PodcastsList';
-import PodcastDetail from './views/PodcastDetail/PodcastDetail'
-import EpisodeDetail from './views/EpisodeDetail/EpisodeDetail';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import HomeLayout from "./layout/HomeLayout/HomeLayout";
+import PodcastList from "./views/PodcastsList/PodcastsList";
+import PodcastDetail from "./views/PodcastDetail/PodcastDetail";
+import EpisodeDetail from "./views/EpisodeDetail/EpisodeDetail";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomeLayout/>}>
-           <Route index element={<PodcastList />} />
-           <Route path={'/podcast/:podcastId'} element={<PodcastDetail/> }/>
-           <Route path={'/podcast/:podcastId/episode/:episodeId'} element={<EpisodeDetail/> }/>
-          </Route >
-
+          <Route path="/" element={<HomeLayout />}>
+            <Route index element={<PodcastList />} />
+            <Route path={"/podcast/:podcastId"} element={<PodcastDetail />} />
+            <Route
+              path={"/podcast/:podcastId/episode/:episodeId"}
+              element={<EpisodeDetail />}
+            />
+          </Route>
         </Routes>
-
       </Router>
-
     </div>
   );
 }
